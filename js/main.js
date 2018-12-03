a84a060c74fbf4fe38885b5e0da18ed167c77965
Cbazil <chadbosch08@gmail.com>
2018-12-03 16:23:12 +0200

    Working on Counter

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* ========= Counter functionality | Start ======== */
+
+var countOfCoffee=0;
+var countOfGirls=280;
+var countOfProjects=2680;
+var countOfHours=8265;
+
+var cupsOfCoffee=document.getElementById("cupsOfCoffee");
+var girls=document.getElementById("girls");
+var projects=document.getElementById("projects");
+var hoursCoding=document.getElementById("hoursCoding");
