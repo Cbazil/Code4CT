b6193c3718e3ddecc431ab9986cf5c14d1f380d1
Cbazil <chadbosch08@gmail.com>
2018-12-04 11:55:54 +0200

    Working on Counter section, made variables

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 /* ========= Counter functionality | Start ======== */
-
+'use strict'
 var countOfCoffee=0;
 var countOfGirls=280;
 var countOfProjects=2680;
 var countOfHours=8265;
 
+
 var cupsOfCoffee=document.getElementById("cupsOfCoffee");
-var girls=document.getElementById("girls");
-var projects=document.getElementById("projects");
+var numberofGirls=document.getElementById("numberofGirls");
+var projectsCompleted=document.getElementById("projectsCompleted");
 var hoursCoding=document.getElementById("hoursCoding");
+
+// var timer = setInterval (function(){
+//     cupsOfCoffee.innerHTML=countOfCoffee;
+//     numberofGirls.innerHTML=countOfGirls;
+//     projectsCompleted.innerHTML=countOfProjects;
+//     hoursCoding.innerHtml=countOfHours;
+
+//     if (countOfCoffee===3000) {
+//         clearInterval (timer);
+//         cupsOfCoffee.innerHTML="3000+";
+//         numberofGirls.innerHTML="780+";
+//         projectsCompleted.innerHTML="3120+";
+//         hoursCoding.innerHTML="8765+";
+//     }
+//     countOfCoffee+=10;
+//     countOfGirls++;
+//     countOfProjects++;
+//     countOfProjects++;
+//     },
+// 1);
+
+
